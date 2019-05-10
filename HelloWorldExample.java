a22b9b906c990ec0b85083f94a9bf3740f5cb95c
Apoorv Shrivastava <50495584+ApoorvShrivastava@users.noreply.github.com>
2019-05-10 22:04:12 +0530

    Hello World Java Program

---
+++ HelloWorldExample.java
@@ -0,0 +1,10 @@
+/** This is a traditional Hello World program using Java.
+  * This program prints a message - "Hello World !" on the screen.
+*/
+class HelloWorldExample
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World !");
+	}
+}
